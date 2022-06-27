5da67f5214ebca75f2339b1d6d6957cdb15026a8
VICTOR-OLULETI <oluletiv@gmail.com>
2022-06-27 21:45:11 +0100

    introduction to higher level programmming

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if linked list is circular
+ * @head: head of linked list
+ * Return: 0 if not circular or 1 if circular.
+ */
+int check_cycle(listint_t *head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return (0);
+	current = head;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		if (current == head)
+			return (1);
+	}
+	return (0);
+}
