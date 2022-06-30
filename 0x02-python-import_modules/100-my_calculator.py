dc7cbb206a1861557200f67a365fe7e8fdcef03e
OLULETI VICTOR <oluletiv@gmail.com>
2022-06-30 13:12:10 -0700

    imports all functions from calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
     argc = len(argv)
-    
+
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
