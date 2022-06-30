618f09ce01277f56fb94e1cc446e81f1c5efb7c5
OLULETI VICTOR <oluletiv@gmail.com>
2022-06-30 12:43:15 -0700

    imports the variable a from file variable_load_5.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for i in range(0, len(names)):
-        if names[i][:2] != "__":
-            print("{}".format(names[i]))
+    for i in names:
+        if i[:2] != "__":
+            print("{}".format(i))
