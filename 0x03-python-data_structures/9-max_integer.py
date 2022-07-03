e238a3c6f295322fcc3cbeba48a31bce249077a2
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-03 11:49:42 -0700

    find the biggest integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_num = 0
-    for i in my_list:
-        if i > max_num:
-            max_num = i
-    return (None if not my_list or len(my_list) <= 0 else max_num)
+    if len(my_list) == 0:
+        return None
+    max_num = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+    return (max_num)
