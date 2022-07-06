c74a689b455092c4a0c1adb8fdb7cf1e7e224330
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-05 21:13:01 -0700

    checks if a linked list is  a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,58 @@
 #include "lists.h"
 
 /**
+ * reverse - reverses the linked list
+ * @head: pointer to pointer to head
+ * Return: void
+ */
+
+void reverse(listint_t **head)
+{
+	listint_t *temp, *curr, *prev;
+
+	curr = *head;
+	prev = NULL;
+	while (curr != NULL)
+	{
+		temp = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = temp;
+	}
+	*head = prev;
+}
+
+/**
+ * compareLists - checks the input data for each linked list
+ * @head1: head of first list
+ * @head2: head of second list
+ * Return: 1 if the same data is present in each list else  0.
+ */
+
+bool compareLists(listint_t *head1, listint_t *head2)
+{
+	listint_t *h1 = head1;
+	listint_t *h2 = head2;
+
+
+	while (h1 && h2)
+	{
+		if (h1->n == h2->n)
+		{
+			h1 = h1->next;
+			h2 = h2->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	if (h1 == NULL && h2 == NULL)
+		return (1);
+	return (0);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to pointer to the head of the linked list
  *
@@ -9,38 +61,42 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *step;
-	listint_t *current;
+	listint_t *slow_ptr = *head, *fast_ptr = *head;
+	listint_t *second_half, *prev_slow_ptr = *head;
+	listint_t *middlenode = NULL;
+	bool res = true;
 
-	int n = 0, k, i;
-
-	if (*head == NULL)
-		return (1);
-
-	current = *head;
-	while (current != NULL)
+	if (*head != NULL && (*head)->next != NULL)
 	{
-		current = current->next;
-		n++;
-	}
-	k = n / 2;
-	step = *head;
-	current = *head;
-	while (k != 0)
-	{
-		for (i = 1; i < n; i++)
-			step = step->next;
-		if (current->n == step->n)
+		while (fast_ptr != NULL && fast_ptr->next != NULL)
 		{
-			current = current->next;
-			n = n - 2;
-			step = current;
+			fast_ptr = fast_ptr->next->next;
+			prev_slow_ptr = slow_ptr;
+			slow_ptr = slow_ptr->next;
+		}
+
+		if (fast_ptr != NULL)
+		{
+			middlenode = slow_ptr;
+			slow_ptr = slow_ptr->next;
+		}
+
+		second_half = slow_ptr;
+		prev_slow_ptr->next = NULL;
+		reverse(&second_half);
+		res = compareLists(*head, second_half);
+
+		reverse(&second_half);
+
+		if (middlenode != NULL)
+		{
+			prev_slow_ptr->next = middlenode;
+			middlenode->next = second_half;
 		}
 		else
 		{
-			return (0);
+			prev_slow_ptr->next = second_half;
 		}
-		k = k - 1;
 	}
-	return (1);
+	return (res);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
