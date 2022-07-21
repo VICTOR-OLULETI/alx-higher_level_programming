02026a9ed0f561e4b515f327f0ef3a0d0ab4dea2
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-20 21:38:40 -0700

    size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,6 +2,10 @@
 """Class Square with size validation """
 
 
+class Square:
+    """Class Square with size validation"""
+
+
 def __init__(self, size=0):
     """Constructor of a Square with size"""
     if (type(size) is not int):
