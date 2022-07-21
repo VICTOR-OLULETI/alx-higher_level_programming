61042a25689b5a9aabd80cadf601554aec380e5e
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-21 14:45:25 -0700

    Print Square instance

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""
+Class Square with:
+-size, position (private properties)
+-methods which are area and print_square
+-getters & setters.
+"""
+
+
+class Square:
+    """Class Square with size"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Constructor for Class Square"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+
+        if (type(position) is not tuple) \
+                or (len(position) != 2) \
+                or (type(position[0]) is not int) \
+                or (type(position[1]) is not int) \
+                or (position[0] < 0) \
+                or (position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def area(self):
+        """Method that returns the area of Square"""
+        return (self.__size == 2)
+
+    def my_print(self):
+        """Method to print a Square"""
+        if (self.__size == 0):
+            print()
+        else:
+            for s in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0], end='')
+                print("#" * self.__size)
+
+    def __str__(self):
+        str = ""
+        if (self.__size == 0):
+            return str
+        else:
+            for s in range(self.__position[1]):
+                str += '\n'
+            for i in range(self.__size):
+                str += (" " * self.__position[0])
+                str += "#" * self.__size
+                if (i != self.__size - 1):
+                    str += "\n"
+        return (str)
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """Getter for Private Position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter for position"""
+        if (type(value) is not tuple) \
+                or (len(value) != 2) \
+                or (type(value[0]) is not int) \
+                or (type(value[1]) is not int) \
+                or (value[0] < 0) \
+                or (value[1] < 0):
+            raise TypeError("position must be a tuple of 2-positive integers")
+        else:
+            self.__position = value
