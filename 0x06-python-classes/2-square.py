4f1b824003fd3f362e8b7e7a1b1df1d7e60e08f6
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-20 21:34:16 -0700

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,8 @@
 def __init__(self, size=0):
     """Constructor of a Square with size"""
     if (type(size) is not int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >=0 ")
+        raise (TypeError("size must be an integer"))
+    elif (size < 0):
+        raise (ValueError("size must be >=0 "))
     else:
         self.__size = size
