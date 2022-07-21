538fee62ec7e3715fc804bb5abdc0ff57af68ba8
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-20 21:14:50 -0700

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class Square with size attribute"""
+
+
+class Square:
+    """Creating a field"""
+    def __init__(self, square_size):
+        """Initializing the Square"""
+        self.__size = squre_size
