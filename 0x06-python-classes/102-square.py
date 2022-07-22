2165d45b2cf3267ef68bd42ea97e76ede40d0c2a
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-22 06:21:53 -0700

    Compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Class Square defined by
+-size(private instance)
+-area(public instance)
+"""
+
+
+class Square:
+    """Class Square with size and area"""
+    def __init__(self, size=0):
+        """Construct for Class Square"""
+        if (type(size) is not int) and (type(size) is not float):
+            raise TypeError("size must be a number")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def __eq__(self, other):
+        """Compare operator '==' """
+        return (self.area() == other.area())
+
+    def __ne__(self, other):
+        """Compare operator '!=' """
+        return (self.area() != other.area())
+
+    def __gt__(self, other):
+        """Compare operator '>' """
+        return (self.area() != other.area())
+
+    def __ge__(self, other):
+        """Compare operator '>=' """
+        return (self.area() >= other.area())
+
+    def __lt__(self, other):
+        """Compare operator '<' """
+        return (self.area() < other.area())
+
+    def __le__(self, other):
+        """Compare operator '<=' """
+        return (self.area() <= other.area())
+
+    @property
+    def size(self):
+        """Getter for private attribute size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for private attribute size"""
+        if (type(size) is not int) and (type(size) is not float):
+            raise TypeError("size must be a number")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
