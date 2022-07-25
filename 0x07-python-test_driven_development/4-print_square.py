6776c6b00e982fe726151792afaa15eacec28cfe
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-25 02:45:56 -0700

    Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""My Sqaure module"""
+
+
+def print_square(size):
+    """Prints a Square based on size given"""
+
+    if (type(size) is not int):
+        raise TypeError("size must be an integer")
+
+    if (size < 0):
+        raise ValueError("size must be >= 0")
+
+    if (type(size) is float and size <= 0):
+        raise TypeError("size must be an integer")
+    if size != 0:
+        for i in range(size):
+            print('#' * size)
