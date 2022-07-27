8f07d08f8c1ad6b145659dd6b8e0747dbbaae3ac
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-27 02:43:19 -0700

    Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
     def __del__(self):
         """"Prints message when an instance of Rectangle is deleted"""
         print("Bye rectangle...")
-        Rectangle.numbeer_of_instances -= 1
+        Rectangle.number_of_instances -= 1
 
     def area(self):
         """Calculates Area of rectangle: width * height"""
@@ -91,7 +91,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if (not isinstance(rect_2, Rectangle)):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if (Rectangle.area(rect_1) >= Rectangle.area(rect_2)):
+        if (rect_1.area() >= rect_2.area()):
             return (rect_1)
         else:
             return (rect_2)
