88ed09af47ae41935b31c190198357c83c00a4da
OLULETI VICTOR <oluletiv@gmail.com>
2022-08-01 16:24:08 -0700

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This program raises an exception and integer
+    validation
+"""
+
+
+class BaseGeometry:
+    """
+    creates the area and integer_validator
+    methods
+    """
+
+    def area(self):
+        """
+        raises an Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        raises an exception if error is found
+        Args:
+            name: string
+            value: integer
+        """
+        if (type(value) is not int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if (value <= 0):
+             raise ValueError("{} must be greater than 0".format(name))
