01a5443c11a1ffa58d2d98feeff6495620ee1b7d
OLULETI VICTOR <oluletiv@gmail.com>
2022-08-01 16:57:49 -0700

    creates class that inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This program inherits a class"""
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle:
+        Args:
+            width: positive int
+            height: positive int
+    """
+    def __init__(self, width, height):
+        """Constructor for width and height"""
+        BaseGeometry.integer_validator(self, "", width)
+        self.__width = width
+        BaseGeometry.integer_validator(self, "", height)
+        self.__height = height
