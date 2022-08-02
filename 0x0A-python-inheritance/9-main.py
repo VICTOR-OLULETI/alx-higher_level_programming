c4a467a8e35e9249049e09a7b3e61f3ab7acf534
OLULETI VICTOR <oluletiv@gmail.com>
2022-08-01 17:41:24 -0700

    create full rectangle

---
+++ 0x0A-python-inheritance/9-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+c = Rectangle(3, 5)
+
+print(c)
+print(c.area())
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This program creates a class and demonstrates
+inheritance
+"""
+
+
+BaseGeometry = __import__('8-rectangle').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle inherits the BaseGeometry
+        Args:
+            width: integer
+            height: integer
+    """
+    def __init__(self, width, height):
+        """
+        Constructor of an instance of class Rectangle
+        """
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
+
+    def area(self):
+        """
+        area of the rectangle
+        """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """
+        Returns string representation of class Rectangle
+        """
+        value = "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        return value
