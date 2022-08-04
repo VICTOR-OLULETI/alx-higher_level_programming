e617c0b773cb1d3769f7032803dcddc2e3c2781e
OLULETI VICTOR <oluletiv@gmail.com>
2022-08-03 22:03:49 -0700

    python -input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,14 +4,17 @@ This program defines a Student
 """
 
 
-class Student():
+class Student:
     """
     Class of a student
     """
     def __init__(self, first_name, last_name, age):
         """
         Constructor of a student
-        with arguments first_name,...
+        Args:
+            first_name: str
+            last_name: str
+            age: int
         """
         self.first_name = first_name
         self.last_name = last_name
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ and then save them to a file
 """
 
 
-import sys
-import os
+from sys import argv
+from os.path import exists
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -14,13 +14,15 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = "add_item.json"
+argc = len(argv)
+
 py_list = []
-if os.path.exists(filename):
+if exists(filename):
     py_list = load_from_json_file(filename)
 
-if len(sys.argv) == 1:
+if argc == 1:
     save_to_json_file(py_list, filename)
 else:
-    for i in range(1, len(sys.argv)):
-        py_list.append(sys.argv[i])
+    for i in range(1, argc):
+        py_list.append(argv[i))
     save_to_json_file(py_list, filename)
