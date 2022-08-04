02b3bec8bcfa1ca68c5e33cf319a1da30d2bf851
OLULETI VICTOR <oluletiv@gmail.com>
2022-08-03 22:30:17 -0700

    Load, add and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -19,10 +19,12 @@ argc = len(argv)
 py_list = []
 if exists(filename):
     py_list = load_from_json_file(filename)
+else:
+    save_to_json_file("", filename)
 
 if argc == 1:
     save_to_json_file(py_list, filename)
 else:
     for i in range(1, argc):
-        py_list.append(argv[i))
+        py_list.append(argv[i])
     save_to_json_file(py_list, filename)
