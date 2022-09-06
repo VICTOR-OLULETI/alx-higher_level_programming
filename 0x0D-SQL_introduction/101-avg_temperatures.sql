d3ab368e16be90d7b8ae7671f46bb7973edfb012
VICTOR-OLULETI <oluletiv@gmail.com>
2022-09-07 00:29:44 +0100

    importing tables into database

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- script displays the average temperature (Fahrenheit)
+-- it is by city ordered by temperature (descending).
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
