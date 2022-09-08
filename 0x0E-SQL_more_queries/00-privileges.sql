10780c2f12ddde14696fe73ba4b6be4e33ac0faa
VICTOR-OLULETI <oluletiv@gmail.com>
2022-09-08 05:51:55 +0100

    count shows by genre

---
+++ 0x0E-SQL_more_queries/00-privileges.sql
@@ -0,0 +1,4 @@
+-- script that lists all privileges of the user_0d_1 and user_0d_2
+-- on the local host
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
--- lists genres
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows -- join tables
+-- Script will lists genres and number of shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows -- Query to join tables
 FROM tv_show_genres
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC, tv_genres_id ASC;
+ORDER BY number_of_shows DESC, tv_genres.id ASC;
