51753317d3bbc8c875f3aadf1bd31c89f72eff48
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-08 17:48:51 +0100

    concat 2 files into 1 file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,7 +4,6 @@ const file1 = process.argv[2];
 const file2 = process.argv[3];
 const file3 = process.argv[4];
 
-console.log(`${file1}`);
 fs.readFile(`${file1}`, 'utf8', readingFile);
 fs.readFile(`${file2}`, 'utf8', readingFile);
 function readingFile (error, data) {
@@ -12,8 +11,7 @@ function readingFile (error, data) {
     console.log(error);
   }
   fs.writeFile(`${file3}`, data, { encoding: 'utf8', flag: 'a+' }, writeFile);
-  /* fs.writeFile(`${file3}`, '\n', {encoding: "utf8", flag:"a+"}, writeFile);
-  */
+  // fs.writeFile(`${file3}`, '\n', {encoding: "utf8", flag:"a+"}, writeFile);
 }
 /*
 function readingFile2(error, data) {
