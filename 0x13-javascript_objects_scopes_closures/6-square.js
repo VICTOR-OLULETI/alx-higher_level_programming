c77d223ce07103a88f4a07ad240ca2b20d0add1c
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-08 12:31:20 +0100

    defines a square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const square = require('./5-square');
+module.exports = class Square extends square {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    let character = c;
+    if (c === undefined) {
+      character = 'X';
+    }
+    let result = '';
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        result += character;
+      }
+      console.log(result);
+      result = '';
+    }
+  }
+};
