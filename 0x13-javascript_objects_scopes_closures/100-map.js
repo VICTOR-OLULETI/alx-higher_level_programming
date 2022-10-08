b28bf206deaf74926818b6ae83e243272af62383
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-08 18:06:59 +0100

    map in javascript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-const list2 = list.map(x => x * list.indexOf(x));
+const list2 = list.map((x, index) => index * x);
 console.log(list);
 console.log(list2);
