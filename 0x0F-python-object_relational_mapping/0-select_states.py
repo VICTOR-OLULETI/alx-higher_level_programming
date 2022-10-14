00003687a37f6767d1ea2edf759483b2ac52d9c3
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-14 10:34:27 +0100

    get the states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,7 @@
 """
 
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import MySQLdb
-from sqlalchemy import select
 
 
 mysql_username = sys.argv[1]
@@ -25,24 +21,10 @@ db = MySQLdb.connect(
         host="localhost", port=3306, passwd=mysql_password,
         db=database_name, user=mysql_username
         )
-# Session = sessionmaker(bind=engine)
-# session = Session()
-# Base = declarative_base()
-# Base.metadata.create_all(engine)
-# conn = engine.connect()
-# trans = conn.begin()
-# r = conn.execute("select states.id, name from states order by states.id")
-# r = conn.execute(s).fetchall()
-# for i in r:
-#    print(i)
-# conn.close()
-# result = session.query(states.id, states.name).all()
-# for i in result:
-#    print(i)
-# s = session.scalars(select(states.c.id)).first()
-# print(s)
 r = db.cursor()
 r.execute("""SELECT states.id, name FROM states
         ORDER BY states.id""")
 for i in r.fetchall():
     print(i)
+r.close()
+db.close()
