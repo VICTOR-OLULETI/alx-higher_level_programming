7ffb9e6a35e15f7e4c5fce0dae30d3820416fd25
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-14 13:44:01 +0100

    safe filter states by user input

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    This module takes in an argument and displays
+    all values in the states table of the database
+    where name matches the argument
+"""
+
+
+import sys
+import MySQLdb
+
+
+def main():
+    """
+        This func prints the state values
+        and avoids sql injection
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_searched = sys.argv[4]
+    db = MySQLdb.connect(
+            host="localhost", port=3306, passwd=mysql_password,
+            db=database_name, user=mysql_username
+            )
+    r = db.cursor()
+    r.execute("""SELECT states.id, name FROM states WHERE BINARY
+                name = (%s) ORDER BY states.id""", (state_searched,))
+    for i in r.fetchall():
+        print(i)
+    r.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    # only to run when not called by import
+    main()
