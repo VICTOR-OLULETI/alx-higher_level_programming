3d4e06ae524d8af0f3b361125309efa0ef791ca4
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-14 16:09:16 +0100

    fetches first result for state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    This module returs the first state object
+    from the specified database.
+    command line arguments:
+        first-argument: username
+        second-argument: password
+        third-argument: database_name
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).order_by(State.id).first()
+    if (state is not None):
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print()
+    session.close()
