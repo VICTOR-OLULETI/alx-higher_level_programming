3784c4333e477d32af355efc4d5b4b862c0153bf
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-14 10:42:15 +0100

    get the states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,23 +8,33 @@ import sys
 import MySQLdb
 
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-"""
-engine = create_engine(
+def states():
+    """
+        This func prints the state values
+        ordered by the state id
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    """
+    engine = create_engine(
         "mysql+mysqldb://"+mysql_username + ":"
         + mysql_password+"@localhost:3306/" + database_name
         )
-"""
-db = MySQLdb.connect(
-        host="localhost", port=3306, passwd=mysql_password,
-        db=database_name, user=mysql_username
-        )
-r = db.cursor()
-r.execute("""SELECT states.id, name FROM states
-        ORDER BY states.id""")
-for i in r.fetchall():
-    print(i)
-r.close()
-db.close()
+    """
+    db = MySQLdb.connect(
+            host="localhost", port=3306, passwd=mysql_password,
+            db=database_name, user=mysql_username
+            )
+    r = db.cursor()
+    r.execute("""SELECT states.id, name FROM states
+            ORDER BY states.id""")
+    for i in r.fetchall():
+        print(i)
+    r.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    # only to run when not called by import
+    states()
