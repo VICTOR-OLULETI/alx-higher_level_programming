5e3966f53aa2b0b5d281bb700431bcba0be61042
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-14 16:10:08 +0100

    fetches first result for state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     if (state is not None):
         print("{}: {}".format(state.id, state.name))
     else:
-        print()
+        print('Nothing')
     session.close()
