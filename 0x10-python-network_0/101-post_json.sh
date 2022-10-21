9643af8eb4302f8dd643351679e00220b822e605
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-21 03:13:56 +0100

    read and post a json file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # using post method on a json type file
-curl -s -X POST "$1" -H 'Content-Type:application/json' -d cat "$2"
+curl -s -X POST "$1" -H 'Content-Type:application/json' -d "@$2"
