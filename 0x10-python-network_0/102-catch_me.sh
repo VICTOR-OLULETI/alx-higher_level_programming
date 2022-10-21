d7c1bf56fa9242ba2bd87328ebc4df67f9fd203e
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-21 03:50:45 +0100

    print response

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # the server responds with 'You got me!' when the url below is loaded
-curl -s -L 0.0.0.0:5000/catch_me -X PUT -H "Origin: You got me!"
+curl -s -L -i 0.0.0.0:5000/catch_me -w 'You Got me!'
