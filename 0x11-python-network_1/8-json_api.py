9f2d2b3b26875857824952ce93a5378a1f41722c
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-22 15:43:11 +0100

    json api with requests library

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     r = requests.post('http://0.0.0.0:5000/search_user', data)
     if (type(r), dict):
         r_dict = r.json()
-        if (r_dict == {}):
+        if (len(r_dict) == 0):
             print("No result")
-        else:
+        elif (r_dict.get('id') is not None):
             print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    else:
-        print("Not a vaild JSON")
+        else:
+            print("Not a vaild JSON")
