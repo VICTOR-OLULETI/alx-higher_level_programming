135f191f63309a8cda36891fb2080d67b7d25244
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-22 15:13:03 +0100

    authentication with requests library

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,8 +5,9 @@ import sys
 import requests
 import requests.auth
 
-user = sys.argv[1]
-pwd = sys.argv[2]
-basic = requests.auth.HTTPBasicAuth(user, pwd)
-r = requests.get('https://api.github.com/user', auth=basic)
-print(r.json().get('id'))
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    basic = requests.auth.HTTPBasicAuth(user, pwd)
+    r = requests.get('https://api.github.com/user', auth=basic)
+    print(r.json().get('id'))
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""This module works with json using the requests library
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    if (len(sys.argv) > 1):
+        q = sys.argv[1]
+    else:
+        q = ""
+    data = {}
+    data['q'] = q
+    r = requests.post('http://0.0.0.0:5000/search_user', data)
+    if (type(r), dict):
+        r_dict = r.json()
+        if (r_dict == {}):
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    else:
+        print("Not a vaild JSON")
