59684e715539f9cb832bcc4be07d0c9fa4fdd64e
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-22 15:11:14 +0100

    authentication using requests library

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module uses basic Authentications
+"""
+import sys
+import requests
+import requests.auth
+
+user = sys.argv[1]
+pwd = sys.argv[2]
+basic = requests.auth.HTTPBasicAuth(user, pwd)
+r = requests.get('https://api.github.com/user', auth=basic)
+print(r.json().get('id'))
