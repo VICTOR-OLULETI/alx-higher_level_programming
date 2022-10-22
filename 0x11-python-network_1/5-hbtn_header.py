c3fc30a5a1cca5cf8954383dba28ae0e4cd455a6
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-22 02:53:16 +0100

    response header value

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module takes in a URL and sends a request to the URL and
+displays the value of the variable X-Request-Id
+"""
+import requests
+import sys
+
+url_input = sys.argv[1]
+r = requests.get(url_input, auth=('user', 'pass'))
+print(r.headers['X-Request-Id'])
