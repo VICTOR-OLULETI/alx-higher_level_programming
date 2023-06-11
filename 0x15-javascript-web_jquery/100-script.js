db101ca6ccbaa8f28a3b7dee627c3b1f0affd414
VICTOR-OLULETI <oluletiv@gmail.com>
2023-06-12 00:15:40 +0100

    document loaded without jquery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* This script updates the text color of the header element to red
+*/
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    header.style.color = '#FF0000';
+  });
+  
