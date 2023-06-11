06444848f4dff2ec8b7e2e7038b1aa0c84b97a7e
VICTOR-OLULETI <oluletiv@gmail.com>
2023-06-11 20:24:30 +0100

    adds elements to a list

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-/* This script adds the class red to the header when a
-click occurs on the tag DIV#red_header
+/* This script toggles between the class red and green to change the header color when click occurs on the tag 
+toggle header
 */
 
 const $toggle = $('#toggle_header');
---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* This script adds a li element to a list when the user clicks on the tag
+div#add_item 
+*/
+
+$('div#add_item').click(function() {
+    $('ul.my_list').append('<li>Item</li>');
+})
